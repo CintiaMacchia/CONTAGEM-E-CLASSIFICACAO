5714344791e8ece99119bff5e5337968012fcdb2
Cintia <macchiapaola@hotmail.com>
2021-09-21 17:29:03 -0300

    atualização

--- index.js
+++ index.js
@@ -1,5 +1,8 @@
 window.alert("Calculo de média bimestral!");
+var professor = window.prompt("Qual nome do professor?");
+var materia = window.prompt("Qual a matéria?");
 var aluno = window.prompt("Qual o nome do aluno?");
+
 
 //variaveis
 var nota1 = parseFloat(window.prompt("Digite o valor da nota do Primeiro Bimestre."));
@@ -9,10 +12,10 @@ var nota4 = parseFloat(window.prompt("Digite o valor da nota do Quarto Bimestre.
 
 var mediaAno = (nota1 + nota2 + nota3 + nota4)/ 4;
 
-window.alert("A média do " aluno + "é " + mediaAno);
+window.alert("A média é " + mediaAno);
 
 if (mediaAno >= 6){
-    alert("Aluno Aprovado");
+    alert(aluno +  " Aprovado na matéria de " +  materia + "  do professor(a) " +  professor);
 } else{
-    alert("Aluno Reprovado");
+    alert(aluno +  " Reprovado na matéria de " +  materia + "  do professor(a) " +  professor);
 }
